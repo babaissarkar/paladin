c19a6ca6ddbc4f32dafbbabb2f4ad8ea1dab3cdc
babaissarkar <suvrax@gmail.com>
2016-03-02 08:42:34 +0530

    modified:   clz/CDBViewer.java

--- clz/CDBViewer.java
+++ clz/CDBViewer.java
@@ -7,16 +7,14 @@ import java.util.Vector;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.RowSorter;
-import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
+import javax.swing.table.TableColumnModel;
 
 @SuppressWarnings("serial")
 public class CDBViewer extends JFrame {
 	
 	public CDBViewer(Deque<Card> library) {
 		setTitle("Card Database Viewer");
-		setSize(700, 400);
+		setSize(1300, 400);
 		getContentPane().setLayout(new BorderLayout());
 		Vector<Vector<String>> vLib = new Vector<Vector<String>>();
 		for (Card c : library) {
@@ -32,10 +30,25 @@ public class CDBViewer extends JFrame {
 		headers.add("Power");
 		headers.add("Energy Number");
 		headers.add("Effects");
+		headers.add("Effects 2");
+		headers.add("Effects 3");
+		headers.add("Effects 4");
+		headers.add("Effects 5");
+		headers.add("Effects 6");
 		JTable table = new JTable(vLib, headers);
-		RowSorter<TableModel> sorter = new TableRowSorter<TableModel>(table.getModel());
-		table.setRowSorter(sorter);
-		table.setEnabled(false);
+//		RowSorter<TableModel> sorter = new TableRowSorter<TableModel>(table.getModel());
+//		table.setRowSorter(sorter);
+		table.setAutoCreateRowSorter(true);
+		TableColumnModel columns = table.getColumnModel();
+		columns.getColumn(0).setPreferredWidth(50);
+		columns.getColumn(1).setPreferredWidth(215);
+		columns.getColumn(2).setPreferredWidth(50);
+		columns.getColumn(3).setPreferredWidth(90);
+		columns.getColumn(4).setPreferredWidth(90);
+		columns.getColumn(5).setPreferredWidth(90);
+		columns.getColumn(6).setPreferredWidth(50);
+		columns.getColumn(7).setPreferredWidth(120);
+//		table.setEnabled(false);
 		JScrollPane scroll = new JScrollPane(table,
 				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
